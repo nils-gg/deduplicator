3143eb10525613293846a4156752e324c03b8c10
Nils Gerstner <nils@gerstner.se>
2017-06-19 13:44:31 +0200

    add streamBuffer in HashSumFile add new ThreadManager class

---
+++ src/com/nilsgg/deduplicator/ThreadManager.java
@@ -0,0 +1,10 @@
+package com.nilsgg.deduplicator;
+
+/**
+ * Created by Nils Gerstner on 2017-06-19.
+ */
+public class ThreadManager {
+    /*
+    Test if new threads minimize the time to create new hashes. Spawn or drain threads dynamically.
+     */
+}
